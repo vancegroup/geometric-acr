97415d803cc149465667ca6c546a12f274d81d83
Ryan Pavlik <rpavlik@iastate.edu>
2011-04-14 17:11:40 -0500

    Recognize constraints by function template specialization

--- BinaryConstraintVisitor.h
+++ BinaryConstraintVisitor.h
@@ -26,16 +26,33 @@
 
 namespace boundary_features {
 	class Constraint;
-
+	class BinaryConstraintVisitor;
+	template<typename T, typename U>
+	Constraint * recognizeConstraint(BinaryConstraintVisitor & /*visitor*/, const T& lhs, const U& rhs) {
+		std::cout << "No constraint!" << std::endl << std::endl;
+		return NULL;
+	}
 	class BinaryConstraintVisitor : public boost::static_visitor<Constraint *> {
 		public:
+			BinaryConstraintVisitor() : constraints(0) {}
 			template<typename T, typename U>
 			Constraint * operator()(const T& lhs, const U& rhs) {
 				std::cout << "Left: name=" << lhs.name << std::endl;
 				std::cout << "Right: name=" << rhs.name << std::endl;
-				return NULL;
-			}		
+
+				return recognizeConstraint(*this, lhs, rhs);
+			}
+
+			int constraints;
+
 	};
+
+	template<>
+	Constraint * recognizeConstraint<Circle, Cylinder>(BinaryConstraintVisitor & visitor, const Circle& lhs, const Cylinder& rhs) {
+		std::cout << "Got a concentric constraint! (circle, cylinder)" << std::endl << std::endl;
+		visitor.constraints++;
+		return NULL;
+	}
 
 } // end of namespace boundary_features
 
